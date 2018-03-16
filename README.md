2bee9241d7c6c97e10e53f05dccbb61434abfca6
Wong Jia Hau <hou32hou@gmail.com>
2018-03-16 08:38:06 +0800

    Added app.js

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # tumpang-backend
+
+# How to start mysql?
+```
+# Refer https://stackoverflow.com/questions/32360687/connect-to-docker-mysql-container-from-localhost 
+docker-composer up
+mysql -h 127.0.0.1 -P 3306 -u root
+```
+
+# How to connect to gcloud vm instance?
+```
+gcloud compute ssh server2
+```
+# How to send query to neo4j in shell?
+```
+curl -H "Content-Type: application/json" -X POST -d \ 
+'{"statements":[{"statement":"match (n) return n"}]}' \ 
+http://localhost:7474/db/data/transaction/commit 
+```
+
+# How to open neo4j Cypher shell?
+```
+docker exec --interactive --tty <container_id> bin/cypher-shell
+```
+
+# How to run neo4j in docker?
+```
+ sudo docker run \                                                             
+    --publish=7474:7474 --publish=7687:7687 \
+    --volume=$HOME/neo4j/data:/data \
+    --volume=$HOME/neo4j/logs:/logs \
+    --env NEO4J_AUTH=none \
+    neo4j
+```
+
+# What to do if deploy fails?
+You probably haven't logined.
+```
+# https://stackoverflow.com/questions/39594478/do-not-have-permission-to-access-app-while-deploying-google-service-account
+gcloud auth login
+```
+
+# How to redirect port in Apache2?
+https://stackoverflow.com/questions/8541182/apache-redirect-to-another-port
+
+# How to run the app?
+```
+cd app/
+nodejs app.js
+```
---
+++ src/app.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : '',
+  database : 'tumpang',
+  protocol : 'tcp',
+  port     : '3306'
+});
+
+
+// [START app]
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+        // connection.connect();
+
+        connection.query('select * from User', function (error, results, fields) {
+                if (error) throw error;
+                console.log('The users are: ', results);
+                res.status(200).send(results).end();
+        });
+
+        //connection.end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '127.0.0.1', () => {
+  // console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
