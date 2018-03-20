c9fbaf95e3f0e5d6e084505931f63e3d451d6cd8
Wong Jia Hau <hou32hou@gmail.com>
2018-03-20 10:40:21 +0800

    Update

--- src/makeInitialRides.js
+++ src/makeInitialRides.js
@@ -3,10 +3,10 @@ const sendQueryToNeo4j = require('./neo4jdb').sendQueryToNeo4j;
 
 // We will assume that neo4j db is populated
 function makeInitialRides() {
-    sendQueryToNeo4j("match (n:User) return n;", (err, res, body) => {
-        console.log(JSON.stringify(body));
-    });
-
+    populateNeo4jFromMysql();
+    // sendQueryToNeo4j("match (n:User) return n;", (err, res, body) => {
+    //     console.log(JSON.stringify(body));
+    // });
 }
 
 // This function is only for testing
@@ -14,14 +14,15 @@ function populateNeo4jFromMysql() {
     connection.query('select * from user', (error, results, fields) => {
         populateUserNodeToNeo4j(results);
     });
+
+    function populateUserNodeToNeo4j(users) {
+        sendQueryToNeo4j("match (n:User) delete n;"); // delete every user
+        users.forEach((r) => {
+            sendQueryToNeo4j(`create (:User${JSON.stringify(r)});`) // add every user as node to neo4j
+        });
+    }
 }
 
-function populateUserNodeToNeo4j(users) {
-    sendQueryToNeo4j("match (n:User) delete n;"); // delete every user
-    users.forEach((r) => {
-        sendQueryToNeo4j("create (:User{id:'" + r.id + "', type:'" + r.type + "'});") // add every user as node to neo4j
-    });
-}
 
 module.exports = {
     makeInitialRides
