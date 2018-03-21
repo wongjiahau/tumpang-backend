d9e90b30af9181a714933cae863d52c01fc5db0d
Wong Jia Hau <hou32hou@gmail.com>
2018-03-21 21:27:54 +0800

    TS-fy neo4jdb

---
+++ src/ts/neo4jdb.ts
@@ -0,0 +1,19 @@
+export class Neo4jDb {
+    public sendQueryToNeo4j(query: string, callback: (err: any, res: any, body: any) => void) {
+        const request = require("request");
+        request("http://localhost:7474/db/data/transaction/commit", {
+            json: {
+                statements: [
+                    {
+                        statement: query,
+                    },
+                ],
+            },
+            method: "POST",
+        }, (error: any, response: any, body: any) => {
+            if (callback) {
+                callback(error, response, body);
+            }
+        });
+    }
+}
