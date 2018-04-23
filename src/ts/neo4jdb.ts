392162ef5019f2c8f425e52f046c61341a51bab5
Wong Jia Hau <hou32hou@gmail.com>
2018-04-23 16:34:09 +0800

    Updated neo4jdb controller to use await/async instead of callback - this result in a more readable code man!

--- src/ts/neo4jdb.ts
+++ src/ts/neo4jdb.ts
@@ -3,34 +3,35 @@ import { IDriver } from "./models/driver";
 import {IRider} from "./models/rider";
 import { parseSchedule } from "./models/schedule";
 export interface INeo4jDb {
-    sendQueryToNeo4j(query: string, callback: (err: any, res: any, body: any) => void): void;
-    fetchRiders(callback: (err: any, riders: IRider[]) => void): void;
+    sendQueryToNeo4j(query: string): Promise<any>;
+    fetchRiders(): Promise<IRider[]>;
     fetchDrivers(callback: (err: any, drivers: IDriver[]) => void): void;
 }
+
 export class Neo4jDb implements INeo4jDb {
-    public sendQueryToNeo4j(query: string, callback: (err: any, res: any, body: any) => void) {
-        const request = require("request");
-        request("http://localhost:7474/db/data/transaction/commit", {
-            json: {
-                statements: [
-                    {
-                        statement: query
-                    }
-                ]
-            },
-            method: "POST"
-        }, (error: any, response: any, body: any) => {
-            if (callback) {
-                callback(error, response, body);
-            }
+    public sendQueryToNeo4j(query: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            const request = require("request");
+            request("http://localhost:7474/db/data/transaction/commit", {
+                json: {
+                    statements: [{ statement: query }]
+                },
+                method: "POST"
+            }, (error: any, response: any, body: any) => {
+                if (error) {
+                     reject(error);
+                } else {
+                     resolve(body);
+                }
+            });
         });
     }
 
-    public fetchRiders(callback: (error: any, riders: IRider[]) => void) {
-        this.sendQueryToNeo4j("MATCH (rider:User{type:'rider'}) return rider;", (err, res, body) => {
-            const data: any[] = body.results[0].data;
-            const riders: any[] = [];
-            data.forEach((x) => {
+    public async fetchRiders(): Promise<IRider[]> {
+        const body = await this.sendQueryToNeo4j("MATCH (rider:User{type:'rider'}) return rider;");
+        const data: any[] = body.results[0].data;
+        const riders: any[] = [];
+        data.forEach((x) => {
                 const row = x.row[0];
                 const r: IRider = {
                     id:        row.id,
@@ -44,36 +45,38 @@ export class Neo4jDb implements INeo4jDb {
                 };
                 riders.push(r);
             });
-            callback(err, riders);
+        return new Promise<IRider[]>((resolve) => {
+            resolve(riders);
         });
     }
 
-    public fetchDrivers(callback: (error: any, drivers: IDriver[]) => void) {
-        this.sendQueryToNeo4j("MATCH (driver:User{type:'driver'})-[:OWNS]->(car) return driver, car;", (err, res, body) => {
-            const data: any[] = body.results[0].data;
-            const drivers: any[] = [];
-            data.forEach((x) => {
-                const driverData = x.row[0];
-                const carData = x.row[1];
-                const r: IDriver = {
-                    id:        driverData.id,
-                    name:      driverData.name,
-                    phone:     driverData.phone,
-                    address:   driverData.address,
-                    schedule:  parseSchedule(driverData.schedule),
-                    departure: Coordinate.parse(driverData.departure),
-                    arrival:   Coordinate.parse(driverData.arrival),
-                    type:      driverData.type,
-                    car: {
-                        model   : carData.model,
-                        capacity: carData.capacity,
-                        plateNum: carData.platenum,
-                        color   : carData.color
-                    }
-                };
-                drivers.push(r);
-            });
-            callback(err, drivers);
+    public async fetchDrivers(): Promise<IDriver[]> {
+        const body = await this.sendQueryToNeo4j("MATCH (driver:User{type:'driver'})-[:OWNS]->(car) return driver, car;");
+        const data: any[] = body.results[0].data;
+        const drivers: any[] = [];
+        data.forEach((x) => {
+            const driverData = x.row[0];
+            const carData = x.row[1];
+            const r: IDriver = {
+                id:        driverData.id,
+                name:      driverData.name,
+                phone:     driverData.phone,
+                address:   driverData.address,
+                schedule:  parseSchedule(driverData.schedule),
+                departure: Coordinate.parse(driverData.departure),
+                arrival:   Coordinate.parse(driverData.arrival),
+                type:      driverData.type,
+                car: {
+                    model   : carData.model,
+                    capacity: carData.capacity,
+                    plateNum: carData.platenum,
+                    color   : carData.color
+                }
+            };
+            drivers.push(r);
+        });
+        return new Promise<IDriver[]>((resolve) => {
+            resolve(drivers);
         });
     }
 
--- src/ts/tests/neo4jdb.test.ts
+++ src/ts/tests/neo4jdb.test.ts
@@ -2,65 +2,59 @@ import {expect} from "chai";
 import {Neo4jDb} from "./../neo4jdb";
 describe("neo4jdb", () => {
     describe("fetchRiders", () => {
-        it("case 1", (done) => {
+        it("case 1", async () => {
             const db = new Neo4jDb();
-            db.fetchRiders((err, riders) => {
-                expect(riders).to.have.lengthOf(8);
-                expect(riders[0]).to .deep.eq({
-                    id: "u2",
-                    name: "peter",
-                    phone: "0123478579",
-                    address: "jalan pjs 10/15,bandar sunway,46150 petaling jaya,selangor ",
-                    schedule: {
-                       1 : { startTime: 900, endTime: 1800 },
-                       2 : { startTime: 900, endTime: 1800 },
-                       3 : { startTime: 900, endTime: 1800 },
-                       4 : { startTime: 900, endTime: 1800 },
-                       5 : { startTime: 900, endTime: 1700 },
-                       6 : { startTime: 900, endTime: 1600 },
-                       7 : { startTime: NaN, endTime: NaN }
-                    },
-                    departure: { latitude: 3.0756886, longitude: 101.60675049999998 },
-                    arrival: { latitude: 3.1615, longitude: 101.69799999999998 } ,
-                    type: "rider"
-                });
-                done();
+            const riders = await db.fetchRiders();
+            expect(riders).to.have.lengthOf(9);
+            expect(riders[0]).to .deep.eq({
+                id: "u2",
+                name: "peter",
+                phone: "0123478579",
+                address: "jalan pjs 10/15,bandar sunway,46150 petaling jaya,selangor ",
+                schedule: {
+                    1 : { startTime: 900, endTime: 1800 },
+                    2 : { startTime: 900, endTime: 1800 },
+                    3 : { startTime: 900, endTime: 1800 },
+                    4 : { startTime: 900, endTime: 1800 },
+                    5 : { startTime: 900, endTime: 1700 },
+                    6 : { startTime: 900, endTime: 1600 },
+                    7 : { startTime: NaN, endTime: NaN }
+                },
+                departure: { latitude: 3.0756886, longitude: 101.60675049999998 },
+                arrival: { latitude: 3.1615, longitude: 101.69799999999998 } ,
+                type: "rider"
             });
         });
     });
 
     describe("fetchDrivers", () => {
-        it("case 1", (done) => {
+        it("case 1", async () => {
             const db = new Neo4jDb();
-            db.fetchDrivers((err, drivers) => {
-                expect(drivers).to.have.lengthOf(2);
-                expect(drivers[0]).to .deep.eq(
-                    {
-                        id: "u1",
-                        name: "jackson",
-                        phone: "012345679",
-                        address: "84,, 60, lorong pjs 10/24a, bandar sunway, 46150 petaling jaya, selangor",
-                        schedule: {
-                           1: { startTime: 830, endTime: 1800 },
-                           2: { startTime: 830, endTime: 1800 },
-                           3: { startTime: 830, endTime: 1800 },
-                           4: { startTime: 830, endTime: 1800 },
-                           5: { startTime: 830, endTime: 1700 },
-                           6: { startTime: 830, endTime: 1500 },
-                           7: { startTime: NaN, endTime: NaN }
-                        },
-                        departure: { latitude: 3.0780289, longitude: 101.60655040000006 } ,
-                        arrival: { latitude: 3.1615, longitude: 101.69799999999998 },
-                        type: "driver",
-                        car: {
-                            model: "myvi",
-                            capacity: 4,
-                            plateNum: "wre 8907",
-                            color: "yellow"
-                        }
-                    }
-                );
-                done();
+            const drivers = await db.fetchDrivers();
+            expect(drivers).to.have.lengthOf(2);
+            expect(drivers[0]).to .deep.eq({
+                id: "u1",
+                name: "jackson",
+                phone: "012345679",
+                address: "84,, 60, lorong pjs 10/24a, bandar sunway, 46150 petaling jaya, selangor",
+                schedule: {
+                    1: { startTime: 830, endTime: 1800 },
+                    2: { startTime: 830, endTime: 1800 },
+                    3: { startTime: 830, endTime: 1800 },
+                    4: { startTime: 830, endTime: 1800 },
+                    5: { startTime: 830, endTime: 1700 },
+                    6: { startTime: 830, endTime: 1500 },
+                    7: { startTime: NaN, endTime: NaN }
+                },
+                departure: { latitude: 3.0780289, longitude: 101.60655040000006 } ,
+                arrival: { latitude: 3.1615, longitude: 101.69799999999998 },
+                type: "driver",
+                car: {
+                    model: "myvi",
+                    capacity: 4,
+                    plateNum: "wre 8907",
+                    color: "yellow"
+                }
             });
         });
     });
